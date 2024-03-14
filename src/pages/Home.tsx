fe45ba91e1b02ec5cab5478fa001a1b06fd52778
Nirnay1037823 <nirnay.1behera@gmail.com>
2024-03-14 11:50:27 +0530

    changed title sankalp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home: React.FC<homeProps> = ({ users }) => {
   }
   return (
     <div className="App">
-      <h1>Monster Rollodex</h1>
+      <h1>Monster Rollodex Sankalp Commit</h1>
       <SearchBar handleChange={handleChange}/>
       <CardList monsters={filterMonsters} />
     </div>
